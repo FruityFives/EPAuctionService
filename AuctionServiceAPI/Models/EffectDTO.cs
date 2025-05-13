9317f5d385b8a2388434e8f2c75478117cdb23d6
jabir8210 <jabir8210@gmail.com>
2025-05-13 13:25:46 +0200

    Changed

--- AuctionServiceAPI/Models/EffectDTO.cs
+++ AuctionServiceAPI/Models/EffectDTO.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Auction
 {
-    public class AuctionEffect
+    public class EffectDTO
     {
         public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
